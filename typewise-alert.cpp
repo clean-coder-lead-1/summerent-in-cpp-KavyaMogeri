32b9dddb0927d6eeed41901a8c3c62f6e6dd0179
kmo6kor <kavya.mogeri@in.bosch.com>
2023-06-16 11:10:50 +0530

    Modify existing code to  adhere to cleancode

--- typewise-alert.cpp
+++ typewise-alert.cpp
@@ -1,35 +1,34 @@
 #include "typewise-alert.h"
 #include <stdio.h>
 
-BreachType inferBreach(double value, double lowerLimit, double upperLimit) {
+const double lowerLimit = 0;
+ 
+BreachType inferBreach(double value, double upperLimit) {
+  BreachType breachType = NORMAL;
   if(value < lowerLimit) {
-    return TOO_LOW;
+    breachType = TOO_LOW;
   }
   if(value > upperLimit) {
-    return TOO_HIGH;
+    breachType = TOO_HIGH;
   }
-  return NORMAL;
+  return breachType;
 }
 
 BreachType classifyTemperatureBreach(
     CoolingType coolingType, double temperatureInC) {
-  int lowerLimit = 0;
   int upperLimit = 0;
   switch(coolingType) {
     case PASSIVE_COOLING:
-      lowerLimit = 0;
       upperLimit = 35;
       break;
     case HI_ACTIVE_COOLING:
-      lowerLimit = 0;
       upperLimit = 45;
       break;
     case MED_ACTIVE_COOLING:
-      lowerLimit = 0;
       upperLimit = 40;
       break;
   }
-  return inferBreach(temperatureInC, lowerLimit, upperLimit);
+  return inferBreach(temperatureInC, upperLimit);
 }
 
 void checkAndAlert(
