b68e1a17dd3f1bf018208d341705fa7423321d9a
kmo6kor <kavya.mogeri@in.bosch.com>
2023-06-16 15:08:23 +0530

    Modify code

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -14,3 +14,15 @@ TEST_CASE("infers the breach according to limits") {
 TEST_CASE("infers the breach according to limits") {
   REQUIRE(inferBreach(30, 30) == NORMAL);
 }
+
+TEST_CASE("infers the breach according to limits") {
+  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 42) == TOO_HIGH);
+}
+
+TEST_CASE("infers the breach according to limits") {
+  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, 12) == NORMAL);
+}
+
+TEST_CASE("infers the breach according to limits") {
+  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, 35) == NORMAL);
+}
