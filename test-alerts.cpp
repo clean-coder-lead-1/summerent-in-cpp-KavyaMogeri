81451ad02fe64b1958366791fe277a9606a28e46
kmo6kor <kavya.mogeri@in.bosch.com>
2023-06-19 13:54:21 +0530

    modify code

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -3,26 +3,26 @@
 #include "test/catch.hpp"
 #include "typewise-alert.h"
 
-TEST_CASE("infers the breach according to limits") {
+TEST_CASE("infers the breach for Low temperature") {
   REQUIRE(inferBreach(-12, 30) == TOO_LOW);
 }
 
-TEST_CASE("infers the breach according to limits") {
+TEST_CASE("infers the breach for high temperature") {
   REQUIRE(inferBreach(33, 30) == TOO_HIGH);
 }
 
-TEST_CASE("infers the breach according to limits") {
+TEST_CASE("infers the breach normal temperature") {
   REQUIRE(inferBreach(30, 30) == NORMAL);
 }
 
-TEST_CASE("infers the breach according to limits") {
+TEST_CASE("classify breach Temperature for passive coolong ") {
   REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 42) == TOO_HIGH);
 }
 
-TEST_CASE("infers the breach according to limits") {
+TEST_CASE("classify breach Temperature for active coolong") {
   REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, 12) == NORMAL);
 }
 
-TEST_CASE("infers the breach according to limits") {
+TEST_CASE("classify breach Temperature for med active coolong") {
   REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, 35) == NORMAL);
 }
